eb99a350657e90cc9c771864d236b19385d342a2
Sydney Sailors <syd.sailors@gmail.com>
2025-07-16 13:53:41 -0500

    Update .gitignore, app.py, requirements.txt files

--- app.py
+++ app.py
@@ -1,1 +1,132 @@
+# --------------------------------------------
+# Imports at the top - PyShiny EXPRESS VERSION
+# --------------------------------------------
 
+# From shiny, import just reactive and render
+from shiny import reactive, render
+
+# From shiny.express, import just ui
+from shiny.express import ui
+
+# Imports from Python Standard Library to simulate live data
+import random
+from datetime import datetime
+
+# --------------------------------------------
+# Optional: Import font awesome icons as you like
+# --------------------------------------------
+
+from faicons import icon_svg
+
+# --------------------------------------------
+# FOR OPTIONAL LOCAL DEVELOPMENT
+# --------------------------------------------
+
+# Add all packages not in the Std Library
+# to requirements.txt ONLY when working locally:
+#
+# faicons
+# shiny
+# shinylive
+# 
+# And install them into an active project virtual environment (usually in .venv)
+# --------------------------------------------
+# --------------------------------------------
+# SET UP THE REACTIVE CONTENT
+# --------------------------------------------
+
+# --------------------------------------------
+# PLANNING: We want to get a fake temperature and 
+# Time stamp every N seconds. 
+# For now, we'll avoid storage and just 
+# Try to get the fake live data working and sketch our app. 
+# We can do all that with one reactive calc.
+# Use constants for update interval so it's easy to modify.
+# ---------------------------------------------------------
+
+# --------------------------------------------
+# First, set a constant UPDATE INTERVAL for all live data
+# Constants are usually defined in uppercase letters
+# Use a type hint to make it clear that it's an integer (: int)
+# --------------------------------------------
+UPDATE_INTERVAL_SECS: int = 3
+# --------------------------------------------
+
+# Initialize a REACTIVE CALC that our display components can call
+# to get the latest data and display it.
+# The calculation is invalidated every UPDATE_INTERVAL_SECS
+# to trigger updates.
+
+# It returns everything needed to display the data.
+# Very easy to expand or modify.
+# (I originally looked at REACTIVE POLL, but this seems to work better.)
+# --------------------------------------------
+
+@reactive.calc()
+def reactive_calc_combined():
+
+    # Invalidate this calculation every UPDATE_INTERVAL_SECS to trigger updates
+    reactive.invalidate_later(UPDATE_INTERVAL_SECS)
+
+    # Data generation logic. Get random between -17 and -19 C, rounded to 1 decimal place
+    temperature = round(random.uniform(-17, -19), 1)
+
+    # Get a timestamp for "now" and use string format strftime() method to format it
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    latest_dictionary_entry = {"temp": temperature, "timestamp": time}
+
+    # Return everything we need
+    return latest_dictionary_entry
+
+# ------------------------------------------------
+# Define the Shiny UI Page layout - Page Options
+# ------------------------------------------------
+
+# Call the ui.page_opts() function
+# Set title to a string in quotes that will appear at the top
+# Set fillable to True to use the whole page width for the UI
+
+ui.page_opts(title="PyShiny Express: Live Data (Basic)", fillable=True)
+
+# ------------------------------------------------
+# Define the Shiny UI Page layout - Sidebar
+# ------------------------------------------------
+
+# Sidebar is typically used for user interaction/information
+# Note the with statement to create the sidebar followed by a colon
+# Everything in the sidebar is indented consistently
+
+with ui.sidebar(open="open"):
+    ui.h2("Antarctic Explorer", class_="text-center")
+    ui.p(
+        "A demonstration of real-time temperature readings in Antarctica.",
+        class_="text-center",
+    )
+
+#---------------------------------------------------------------------
+# In Shiny Express, everything not in the sidebar is in the main panel
+#---------------------------------------------------------------------
+
+
+ui.h2("Current Temperature")
+
+@render.text
+def display_temperature():
+    """Get the latest reading and return a temperature string"""
+    latest_dictionary_entry = reactive_calc_combined()
+    return f"{latest_dictionary_entry['temp']} C"
+
+ui.p("warmer than usual")
+
+ui.div(icon_svg("sun"))
+
+ui.hr()
+
+ui.h2("Current Date and Time")
+
+@render.text
+def display_time():
+    """Get the latest reading and return a timestamp string"""
+    latest_dictionary_entry = reactive_calc_combined()
+    return f"{latest_dictionary_entry['timestamp']}"
